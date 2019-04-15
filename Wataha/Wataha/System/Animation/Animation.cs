7d2347bac16d6f1b37f70c682396eb0c13876b9d
Wiktor <wiktor.muraszko@wp.pl>
2019-04-15 18:23:29 +0200

    Animation v1

---
+++ Wataha/Wataha/System/Animation/Animation.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+
+namespace Wataha.System.Animation
+{
+    class Animation
+    {
+        public int CurrentFrame { get; set; }
+        public bool IsLooping { get; set; }
+        public Dictionary<String,Model> animation { get; private set; }
+        public float frameSpeed { get; set; }
+        public ContentManager content { get; set; }
+
+        public Animation(ContentManager content,String animationfolder)
+        {
+            this.content = content;
+            Model animationFrame;
+            animationFrame = content.Load<Model>(animationfolder+"");
+            frameSpeed = 0.2f;
+        }
+    }
+}
---
+++ Wataha/Wataha/System/Animation/AnimationSystem.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wataha.System
+{
+    class AnimationSystem
+    {
+        private Model model;
+        public AnimationSystem() {
+            List<Model> animation = new List<Model>();
+        }
+        public AnimationSystem(Model model)
+        {
+            this.model = model;
+        }
+
+    }
+}
