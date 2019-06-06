03f1b52a29d58c992b9080910079468ec135fa48
AD-7 <dominiak.aleksander@onet.pl>
2019-06-06 15:24:36 +0200

    Update Animation.cs

--- Wataha/Wataha/GameSystem/Animation/Animation.cs
+++ Wataha/Wataha/GameSystem/Animation/Animation.cs
@@ -65,7 +65,7 @@ namespace Wataha.GameSystem.Animation
             {
 
                 Trace.WriteLine("Animation - LaodContent  animation " + obj);
-                if (obj.Equals("tex_0"))
+                if (obj.Contains("tex_0"))
                     continue;
                 String modelPath = animationFolder + "\\" + obj;
                 animation.Add(i, content.Load<Model>(modelPath));
